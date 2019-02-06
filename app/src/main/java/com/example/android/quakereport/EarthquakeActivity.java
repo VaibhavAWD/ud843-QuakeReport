793209d2c6e085a473e5521c9c4f06beafe12fb5
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2019-02-06 13:49:34 +0530

    Load earthquakes making network request using AsyncTask

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,9 +15,14 @@
  */
 package com.example.android.quakereport;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -29,27 +34,49 @@ import com.example.android.quakereport.model.Earthquake;
 import com.example.android.quakereport.utils.QueryUtils;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
+    private static final String REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+
+    private static final String KEY_CURRENT_EARTHQUAKES = "KEY_CURRENT_EARTHQUAKES";
+
+    private List<Earthquake> mEarthquakes;
+
+    @BindView(R.id.list)
+    ListView mEarthquakeListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
+        ButterKnife.bind(this);
 
-        // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = findViewById(R.id.list);
+        /*
+          Display the current earthquakes if the saved instance state is not null
+          and contains list of earthquakes otherwise make network call and retrieve
+          new earthquakes with respect to the request URL.
+         */
+        if (savedInstanceState != null && savedInstanceState.containsKey(KEY_CURRENT_EARTHQUAKES)) {
+            mEarthquakes = savedInstanceState.getParcelableArrayList(KEY_CURRENT_EARTHQUAKES);
+            updateUi();
+        } else {
+            // Check internet connectivity before making network calls
+            if (hasConnection()) {
+                new EarthquakeAsyncTask().execute();
+            } else {
+                Toast.makeText(this, R.string.error_no_internet_connection,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
 
-        // Create a new {@link EarthquakeAdapter} of earthquakes
-        EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this, QueryUtils.extractEarthquakes());
-
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(earthquakeAdapter);
-
-        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mEarthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 Earthquake earthquake = (Earthquake) adapterView.getAdapter().getItem(position);
@@ -60,9 +87,68 @@ public class EarthquakeActivity extends AppCompatActivity {
                 if (browserIntent.resolveActivity(getPackageManager()) != null) {
                     startActivity(browserIntent);
                 } else {
-                    Toast.makeText(EarthquakeActivity.this, R.string.warn_no_app_found, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(EarthquakeActivity.this, R.string.warn_no_app_found,
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    /**
+     * Make HTTP request and fetch earthquakes on Background Thread and publish the
+     * results on the UI Thread using {@link AsyncTask}.
+     */
+    private class EarthquakeAsyncTask extends AsyncTask<Void, Void, List<Earthquake>> {
+
+        @Override
+        protected List<Earthquake> doInBackground(Void... voids) {
+            return QueryUtils.fetchEarthquakes(REQUEST_URL);
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> earthquakes) {
+            mEarthquakes = earthquakes;
+            updateUi();
+        }
+
+    }
+
+    /**
+     * Loads the earthquakes in the ListView.
+     */
+    private void updateUi() {
+        // Create a new {@link EarthquakeAdapter} of earthquakes
+        EarthquakeAdapter earthquakeAdapter =
+                new EarthquakeAdapter(getApplicationContext(), mEarthquakes);
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        mEarthquakeListView.setAdapter(earthquakeAdapter);
+    }
+
+    /**
+     * Returns true or false by checking the internet connectivity.
+     */
+    private boolean hasConnection() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            return networkInfo != null && networkInfo.isConnected();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Store the current earthquakes in order to avoid making
+     * repetitive network calls on config changes.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle currentState) {
+        currentState.putParcelableArrayList(
+                KEY_CURRENT_EARTHQUAKES,
+                (ArrayList<? extends Parcelable>) mEarthquakes);
+        super.onSaveInstanceState(currentState);
     }
 }
--- app/src/main/java/com/example/android/quakereport/model/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/model/Earthquake.java
@@ -1,6 +1,9 @@
 package com.example.android.quakereport.model;
 
-public class Earthquake {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Earthquake implements Parcelable {
     private double mMagnitude;
     private String mPlace;
     private long mTime;
@@ -12,6 +15,25 @@ public class Earthquake {
         mTime = time;
         mUrl = url;
     }
+
+    protected Earthquake(Parcel in) {
+        mMagnitude = in.readDouble();
+        mPlace = in.readString();
+        mTime = in.readLong();
+        mUrl = in.readString();
+    }
+
+    public static final Creator<Earthquake> CREATOR = new Creator<Earthquake>() {
+        @Override
+        public Earthquake createFromParcel(Parcel in) {
+            return new Earthquake(in);
+        }
+
+        @Override
+        public Earthquake[] newArray(int size) {
+            return new Earthquake[size];
+        }
+    };
 
     public double getMagnitude() {
         return mMagnitude;
@@ -27,5 +49,18 @@ public class Earthquake {
 
     public String getUrl() {
         return mUrl;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeDouble(mMagnitude);
+        parcel.writeString(mPlace);
+        parcel.writeLong(mTime);
+        parcel.writeString(mUrl);
     }
 }
